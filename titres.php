21a12915bb3bd15d29402eaf72bc107eb8de7cfb
Jonathan Buso <jonathanbuso.26@gmail.com>
2020-05-29 23:16:54 +0200

    minor adjustemnt titles page controls

--- titres.php
+++ titres.php
@@ -53,21 +53,26 @@
                     <div class="panels first">
                         <?php echo $messageLogin; ?>
                         <h1>Liste des Titres</h1>
-                        <p id="advertise" class="rouge">Vous devez choisir un titre pour activer les boutons</p>
-                        <label>Titre :</label>
-                        <?php
-                            include_once('includes/titres.php');
-                        ?>
-                        <input type="text" id="c_edit_titre" placeholder="Entrez le nouveau titre ici"/>
-                        <input id="edit_titre" class="bouton btnDisabled inline-block" type="button" value="Modifier"/>
-                        <input id="save_titre" class="bouton btnDisabled" type="button" value="Enregistrer"/>
-                        <span id="messageEdit"></span>
-                        <div id="add_titre"><a>Ajouter un titre</a></div>
-                        <br/>
-                        <input type="text" id="c_add_titre" placeholder="Entrez le nouveau titre ici"/>
-                        <input type="button" id="save_add_titre" class="bouton btnDisabled" value="Enregistrer"/>
-                        <span id="messageAdd"></span>
-                        <input id="page" type="hidden" value="titres"/>
+                        <div class="margin-left">
+                            <p id="advertise" class="rouge">Vous devez choisir un titre pour activer les boutons</p>
+                            <label>Titre :</label>
+                            <?php
+                                include_once('includes/titres.php');
+                            ?>
+                            <input type="text" id="c_edit_titre" placeholder="Entrez le nouveau titre ici"/>
+                            <input id="edit_titre" class="bouton inline-block" type="button" value="Modifier" disabled/>
+                            <input id="save_titre" class="bouton" type="button" value="Enregistrer" disabled/>
+                            <span id="messageEdit" class="margin-top"></span>
+                            <div id="add_titre" class="margin-top">
+                                <a>Ajouter un titre</a>
+                            </div>
+                            <br/>
+                            <input type="text" id="c_add_titre" placeholder="Entrez le nouveau titre ici"/>
+                            <input type="button" id="save_add_titre" class="bouton btnDisabled" value="Enregistrer"/>
+                            <span id="messageAdd"></span>
+                            <input id="page" type="hidden" value="titres"/>
+                        </div>
+
                     </div>
 
                 </section>
