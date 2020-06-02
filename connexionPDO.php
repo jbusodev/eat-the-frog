cf3927fe00d5073c975e71fa434157fdb3b13251
Jonathan Buso <jonathanbuso.26@gmail.com>
2020-06-02 13:57:56 +0200

    updated for heroku

--- connexionPDO.php
+++ connexionPDO.php
@@ -1,9 +1,9 @@
 <?php
 //PDO Connection file - EDIT PARAMATERS BELOW IF YOU ARE HOSTING ON YOUR LOCAL MACHINE
-	$PARAM_hote='postgres://nxmrnloptcddpn:42703db7b2bb298ddfd2f2555af5d9ee4f3223da540acbf4db5fe26da96d0b91@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/d7ggb86d69c7un';
-	$PARAM_nom_bd='crapaud';
-	$PARAM_utilisateur='root';
-	$PARAM_mot_passe=''; // ENTER YOUR PASSWORD HERE
+	$PARAM_hote='ec2-54-246-87-132.eu-west-1.compute.amazonaws.com';
+	$PARAM_nom_bd='d7ggb86d69c7un';
+	$PARAM_utilisateur='nxmrnloptcddpn';
+	$PARAM_mot_passe='42703db7b2bb298ddfd2f2555af5d9ee4f3223da540acbf4db5fe26da96d0b91'; // ENTER YOUR PASSWORD HERE
 	//Options
 	$PARAM_options = array (
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
