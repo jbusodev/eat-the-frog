d55119503a15547a8d5e4174a415948339127523
Jonathan Buso <jonathanbuso.26@gmail.com>
2020-06-03 01:00:42 +0200

    switched from PDO to MySQLi for testing

--- connexionPDO.php
+++ connexionPDO.php
@@ -3,8 +3,9 @@
 // mysql://b5bcc1e7cbeba6:64b1232c@eu-cdbr-west-03.cleardb.net/heroku_db94a9d4da5b206?reconnect=true
 	$host='eu-cdbr-west-03.cleardb.net';
 	$dbuser='b5bcc1e7cbeba6';
-	$dbpassword='64b1232c'; // ENTER YOUR PASSWORD HERE
+	$dbpassword='64b1232c';
 	$database='heroku_db94a9d4da5b206';
+	$port="3306";
 	//Options
 	$PARAM_options = array (
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
@@ -12,10 +13,13 @@
 	);
 
 	try{
-		$connexion = new PDO(
-			"mysql:host=$host;dbname=$database", $dbuser, $dbpassword
-		);
-		$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// PDO Connection
+		//	$connexion = new PDO("mysql:host=$host;dbname=$database", $dbuser, $dbpassword);
+		// Error mode
+		// $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$connexion = new mysqli($host, $dbuser, $dbpassword, $database, $port);
+
 	}
 	catch(Exception $e){
 		echo 'Connection failed : '.$e->getMessage().'<br />';
